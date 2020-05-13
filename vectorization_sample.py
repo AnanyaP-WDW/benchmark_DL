add9642a602ae7e12ff4592754e85125ecc5763e
Ananya Pathak <pathakananya95@gmail.com>
2020-05-14 00:40:19 +0530

    added comments

--- vectorization_sample.py
+++ vectorization_sample.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+# checking time for vector multiplication
 a = np.random.rand(1000000)
 b = np.random.rand(1000000)
 t0 = time.time()
@@ -7,7 +8,7 @@ c = np.dot(a,b)
 t1 = time.time()
 print("Vectorized version" +" *** " + str(1000*(t1-t0)) + " ms")
 
- # checking for for loop
+ # checking time for "for" loop
 
 t0 = time.time()
 for i in range(1000000):
